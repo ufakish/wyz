f3b11c4eea1e26d118d86cbd19304e49f04542eb
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-24 19:57:05 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -71,7 +71,7 @@ async def get_thumb(videoid):
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(1.1)
         draw = ImageDraw.Draw(background)
-        arial = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 45)
+        arial = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 46)
         font = ImageFont.truetype("AnonXMusic/assets/font.ttf", 45)
         draw.text((1000, 8), unidecode(app.name), fill="white", font=arial)
     #    draw.text(
@@ -81,9 +81,9 @@ async def get_thumb(videoid):
     #        font=arial,
     #    )
         draw.text(
-            (55, 580),
+            (40, 580),
             clear(title),
-            (0, 0, 0), 
+            (255, 255, 255), 
             font=font,
         )
         draw.line(
@@ -101,13 +101,13 @@ async def get_thumb(videoid):
         draw.text(
             (36, 670),
             "00:00",
-            (0, 0, 0),
+            (255, 255, 255),
             font=arial,
         )
         draw.text(
             (1185, 670),
             f"{duration[:23]}",
-            (0, 0, 0),
+            (255, 255, 255),
             font=arial,
         )
         try:
