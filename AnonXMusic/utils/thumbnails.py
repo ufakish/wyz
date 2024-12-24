b7a2c0f5d2e7b27bac4925182384385eea4a4278
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-24 18:06:48 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageChops, ImageDraw, ImageEnhance, ImageFilter, ImageFo
 from youtubesearchpython.__future__ import VideosSearch
 
 from config import YOUTUBE_IMG_URL
-
+from AnonXMusic import app
 
 
 def changeImageSize(maxWidth, maxHeight, image):
