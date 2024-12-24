4fdab31e2bf9ac1a38d705f4c7f00259006afc57
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-25 02:24:29 +0530

    Update database.py

--- AnonXMusic/utils/database.py
+++ AnonXMusic/utils/database.py
@@ -4,7 +4,7 @@ from datetime import date
 from typing import Dict, List, Union
 
 from AnonXMusic import userbot
-from AnonXMusic import mongodb
+from AviaxMusic.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -72,7 +72,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from AviaxMusic.core.userbot import assistants
+    from AnonXMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -86,7 +86,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from AviaxMusic.core.userbot import assistants
+    from AnonXMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -113,7 +113,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from AviaxMusic.core.userbot import assistants
+    from AnonXMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -126,7 +126,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from AviaxMusic.core.userbot import assistants
+    from AnonXMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
