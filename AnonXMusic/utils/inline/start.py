6e1307ee69b4c12f503c99aefa6a126dd13f247c
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-30 12:37:58 +0000

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -9,8 +9,7 @@ def start_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            ),   
         ],
     ]
     return buttons
@@ -18,10 +17,6 @@ def start_panel(_):
 
 def private_panel(_):
     buttons = [
-        [
-            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
