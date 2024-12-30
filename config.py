52d8d1208d96843b539ef75737bfe80ae991458e
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-30 21:20:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,21 +78,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
+    "START_IMG_URL", "https://envs.sh/o95.jpeg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://envs.sh/o95.jpeg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://envs.sh/o95.jpeg"
+STATS_IMG_URL = "https://envs.sh/o95.jpeg"
+TELEGRAM_AUDIO_URL = "https://envs.sh/o95.jpeg"
+TELEGRAM_VIDEO_URL = "https://envs.sh/o95.jpeg"
+STREAM_IMG_URL = "https://envs.sh/o95.jpeg"
+SOUNCLOUD_IMG_URL = "https://envs.sh/o95.jpeg"
+YOUTUBE_IMG_URL = "https://envs.sh/o95.jpeg"
+SPOTIFY_ARTIST_IMG_URL = "https://envs.sh/o95.jpeg"
+SPOTIFY_ALBUM_IMG_URL = "https://envs.sh/o95.jpeg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://envs.sh/o95.jpeg"
 
 
 def time_to_seconds(time):
