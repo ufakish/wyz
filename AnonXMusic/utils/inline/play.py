8d97bd207b2e3f1816e3bea6b7b9708206f170be
ufakish <phalishort@gmail.com>
2025-01-21 10:45:25 +0000

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -65,6 +65,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(
                 text="▢", callback_data=f"ADMIN Stop|{chat_id}"
             ),
