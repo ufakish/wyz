8942909ddaca05d80214d13642cf416956bcf35f
ufakish <phalishort@gmail.com>
2025-01-22 03:20:09 +0000

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -6,10 +6,14 @@ from AnonXMusic import app
 
 def start_panel(_):
     buttons = [
+        
         [
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),   
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
     ]
     return buttons
